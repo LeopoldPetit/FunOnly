264fb1850d6fbf87a1555e19d79ebd2c5abf61bf
leopoldpetit <leopoldpetit59@gmail.com>
2023-10-21 11:03:16 +0200

    V1.3

--- src/components/HomePage/CardsComponents.jsx
+++ src/components/HomePage/CardsComponents.jsx
@@ -2,7 +2,7 @@
 export function CardsComponents({data}){
     return(
         <a href="#">
-            <div className="max-w-sm bgColor2 border borderColor rounded-lg shadow transform transition-transform hover:scale-105 hover:shadow-lg">
+            <div className="  max-w-sm bgColor2 border borderColor rounded-lg  transform transition-transform hover:scale-105 hover:shadow-lg">
                 <img className="rounded-t-lg" src={data.infos.image} alt="" />
                 <div className="p-5">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight textColor">{data.infos.name}</h5>
--- src/components/HomePage/navebare.jsx
+++ src/components/HomePage/navebare.jsx
@@ -1,15 +1,18 @@
+import {useNavigate} from "react-router-dom";
+
 export function NaveBare(){
+    const navigate = useNavigate()
     return(
         <nav className="colorNaveBar border-gray-200 fixed top-0 z-10 w-screen NaveBorder">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
                 <div className="flex items-center space-x-4 lg:space-x-0">
-                    <img src="/Logo.png" className="h-16 mr-3 borderRounded" alt="Flowbite Logo" />
-                    <img src="/NameLogo.png" className="h-16 mr-3 borderRounded" alt="Flowbite Logo" />
+                    <img src="/Logo.png" className="h-16 mr-3 borderRounded" alt="Flowbite Logo" onClick={() => navigate(-1)} />
+                    <img src="/NameLogo.png" className="h-16 mr-3 borderRounded" alt="Flowbite Logo" onClick={() => navigate(-1)} />
                 </div>
                 <div className="hidden w-full md:block md:w-auto flex-2" id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white   ">
                         <li>
-                            <a href="#" className="block py-2 pl-3 pr-4 text-white bgColor rounded md:bg-transparent md:text-blue-700 md:p-0  " aria-current="page">Home</a>
+                            <button href="#" className="block py-2 pl-3 pr-4 text-white bgColor rounded md:bg-transparent md:text-blue-700 md:p-0  " aria-current="page" onClick={() => navigate(-1)} >Home</button>
                         </li>
 
                     </ul>
